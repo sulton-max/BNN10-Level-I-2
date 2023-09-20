14b6b0524320526f3dbf172370715f80708f8fb3
Max <sultonbek.rakhimov@gmail.com>
2023-09-20 11:46:16 +0500

    Finish class N42

--- N42-C/Program.cs
+++ N42-C/Program.cs
@@ -7,7 +7,7 @@ var fuelStationService = new FuelStationService(fillerService);
 
 var random = new Random();
 var cars = new List<Car>();
-for(var index = 0; index < 100; index++)
+for(var index = 0; index < 2; index++)
     cars.Add(new Car
     {
         FuelLiters = random.Next(30,60)
